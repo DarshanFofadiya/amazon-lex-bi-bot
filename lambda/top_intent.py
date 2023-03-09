a71205ba8674539393b49c277da6af76c05bf4b6
darshan fofadiya <darshanf@amazon.com>
2023-03-08 23:56:23 -0800

    corrected query

--- lambda/top_intent.py
+++ lambda/top_intent.py
@@ -26,7 +26,7 @@ import bibot_userexits as userexits
 TOP_SELECT  = "SELECT merchant_customer_id, SUM(net_ordered_gms_wk9) as net_ordered_gms FROM scenario1"
 #TOP_JOIN    = " WHERE e.event_id = s.event_id AND v.venue_id = e.venue_id AND c.cat_id = e.cat_id AND d.date_id = e.date_id "
 TOP_WHERE   = " WHERE am = {}"
-TOP_ORDERBY = " GROUP BY merchant_customer_id ORDER BY ticket_sales desc "
+TOP_ORDERBY = " GROUP BY merchant_customer_id ORDER BY net_ordered_gms desc "
 BOTTOM_ORDERBY = " GROUP BY am ORDER BY ticket_sales asc "
 TOP_DEFAULT_COUNT = '5'
 
