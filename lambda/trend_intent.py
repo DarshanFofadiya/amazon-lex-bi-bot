684aa8b4d09ff91589cb729c957ad1391c16a270
darshan fofadiya <darshanf@amazon.com>
2023-03-09 17:20:08 -0800

    removed merchant slot from trend intent for testing purposes

--- lambda/trend_intent.py
+++ lambda/trend_intent.py
@@ -25,7 +25,8 @@ import bibot_userexits as userexits
 
 # SELECT statement for Trend query
 TREND_SELECT = "SELECT sum(net_ordered_gms_wk9) as gms_curr_week, sum(net_ordered_gms_wk8) as gms_curr_weekminus1, sum(net_ordered_gms_wk7) as gms_curr_weekminus2, sum(net_ordered_gms_wk6) as gms_curr_weekminus3 from scenario1"
-TREND_WHERE = " where am = {} and  merchant_customer_id = {}"
+#TREND_WHERE = " where am = {} and  merchant_customer_id = {}"
+TREND_WHERE = " where am = {}"
 
 logger = logging.getLogger()
 logger.setLevel(logging.DEBUG)
@@ -74,7 +75,8 @@ def trend_intent_handler(intent_request, session_attributes):
 
     # Build and execute query
     select_clause = TREND_SELECT
-    where_clause = TREND_WHERE.format(("'" + slot_values.get('am') + "'"), ("'" + slot_values.get('merchant') + "'"))
+    #where_clause = TREND_WHERE.format(("'" + slot_values.get('am') + "'"), ("'" + slot_values.get('merchant') + "'"))
+    where_clause = TREND_WHERE.format(("'" + slot_values.get('am') + "'"))
 
     query_string = select_clause + where_clause
     
@@ -87,7 +89,8 @@ def trend_intent_handler(intent_request, session_attributes):
     result_count = len(response['ResultSet']['Rows']) - 1
 
     # Build response text for Lex
-    response_string = 'The last 4 week trend for merchant_id {} is \n'.format(slot_values.get('merchant'))
+    #response_string = 'The last 4 week trend for merchant_id {} is \n'.format(slot_values.get('merchant'))
+    response_string = 'The last 4 week trend for is \n'
     result_count = len(response['ResultSet']['Rows']) - 1
 
     if result_count > 0:
